6b2cab11373d7d1b6b234e8232b3d85d48210c88
Ben Ten <b.m.steponavicius@gmail.com>
2022-07-01 16:47:44 +0300

    [FIX] Like Button not being found

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.url) {
+    if (changeInfo.url && changeInfo.url.includes('youtube.com/watch?v=')) {
         setTimeout(() => {
             chrome.scripting.executeScript({
                 target: { tabId: tabId },
--- content.js
+++ content.js
@@ -1,5 +1,6 @@
-var element = document.getElementById('top-level-buttons-computed').children[0];
-var isLiked = element.classList.contains('style-default-active');
-if (!isLiked) {
-    element.click();
+var like_button = document.getElementsByTagName('ytd-toggle-button-renderer').item(2);
+var is_liked = like_button.classList.contains('style-default-active');
+alert(is_liked);
+if (!is_liked) {
+    like_button.click();
 }
