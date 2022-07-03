4835cfd49caa5493c5f2b30052a325eda905ab4a
Ben Ten <b.m.steponavicius@gmail.com>
2022-07-03 09:59:45 +0300

    [ADD] Observer

--- js/background.js
+++ js/background.js
@@ -1,10 +1,13 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.url && changeInfo.url.includes('youtube.com/watch?v=')) {
+    if (changeInfo.url &&
+        changeInfo.url.includes('youtube.com/watch?v=') &&
+        !(changeInfo.url.includes('&list'))) {
+        console.log('Video detected')
         setTimeout(() => {
             chrome.scripting.executeScript({
                 target: { tabId: tabId },
                 files: ['js/content.js']
             });
-        }, 3000);
+        }, 3000)
     }
 });
--- js/content.js
+++ js/content.js
@@ -1,5 +1,28 @@
-var like_button = document.getElementsByTagName('ytd-toggle-button-renderer').item(2);
-var is_liked = like_button.classList.contains('style-default-active');
-if (!is_liked) {
-    like_button.click();
+function WaitForElement(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
 }
+
+WaitForElement('yt-icon-button#button.style-scope.ytd-toggle-button-renderer.style-text').then((elm) => {
+    const like_button = document.getElementById('top-level-buttons-computed').children[0];
+    var is_liked = like_button.classList.contains('style-default-active');
+    // alert(like_button.classList.toString() + is_liked);
+    if (!is_liked) {
+        like_button.click();
+    }
+});
